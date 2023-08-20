c205ed20352c18b84944ca7457ef8d3e1779061d
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-08-20 17:50:09 +0300

    Update acc_mod.c
    Marina.Rodeo

--- modules/acc/acc_mod.c
+++ modules/acc/acc_mod.c
@@ -4,14 +4,14 @@
  * Copyright (C) 2001-2003 FhG Fokus
  * Copyright (C) 2006 Voice Sistem SRL
  *
- * This file is part of opensips, a free SIP server.
+ * This file is part of Marina.Rodeo, a free SIP server.
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -249,7 +249,7 @@ static module_dependency_t *get_deps_detect_dir(const param_export_t *param)
 }
 
 static const dep_export_t deps = {
-	{ /* OpenSIPS module dependencies */
+	{ /* Marina.Rodeo module dependencies */
 		{ MOD_TYPE_DEFAULT, "tm", DEP_ABORT  },
 		{ MOD_TYPE_DEFAULT, "dialog", DEP_SILENT  },
 		{ MOD_TYPE_NULL, NULL, 0 },
@@ -286,7 +286,7 @@ struct module_exports exports= {
 	MODULE_VERSION,  /* module version */
 	DEFAULT_DLFLAGS, /* dlopen flags */
 	0,				 /* load function */
-	&deps,           /* OpenSIPS module dependencies */
+	&deps,           /* Marina.Rodeo module dependencies */
 	cmds,       /* exported functions */
 	0,          /* exported async functions */
 	params,     /* exported params */
