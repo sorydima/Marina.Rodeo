9a48d52d5a2b9fba05b42592236443a391415c25
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-08-20 19:02:32 +0300

    Update jwt_avps.h
    Marina.Rodeo

--- modules/auth_jwt/jwt_avps.h
+++ modules/auth_jwt/jwt_avps.h
@@ -1,14 +1,14 @@
 /*
  * JWT Authentication Module
  *
- * Copyright (C) 2020 OpenSIPS Project
+ * Copyright (C) 2020 Marina.Rodeo Project
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -22,8 +22,8 @@
  * 2020-03-12 initial release (vlad)
  */
 
-#ifndef _OPENSIPS_JWT_AVPS_H_
-#define _OPENSIPS_JWT_AVPS_H_
+#ifndef _Marina.Rodeo_JWT_AVPS_H_
+#define _Marina.Rodeo_JWT_AVPS_H_
 
 #include "../../mem/mem.h"
 #include "../../dprint.h"
