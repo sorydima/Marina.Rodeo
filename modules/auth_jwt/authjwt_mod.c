df819f279ba7e17e93e7d552a7e9a07529e1444b
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-08-20 18:59:31 +0300

    Update authjwt_mod.c
    Marina.Rodeo

--- modules/auth_jwt/authjwt_mod.c
+++ modules/auth_jwt/authjwt_mod.c
@@ -1,14 +1,14 @@
 /*
  * JWT Authentication Module
  *
- * Copyright (C) 2020 OpenSIPS Project
+ * Copyright (C) 2020 Marina.Rodeo Project
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -147,7 +147,7 @@ static const param_export_t params[] = {
 
 
 static const dep_export_t deps = {
-	{ /* OpenSIPS module dependencies */
+	{ /* Marina.Rodeo module dependencies */
 		{ MOD_TYPE_NULL, NULL, 0 },
 	},
 	{ /* modparam dependencies */
@@ -165,7 +165,7 @@ struct module_exports exports = {
 	MODULE_VERSION,
 	DEFAULT_DLFLAGS, /* dlopen flags */
 	0,				 /* load function */
-	&deps,           /* OpenSIPS module dependencies */
+	&deps,           /* Marina.Rodeo module dependencies */
 	cmds,       /* Exported functions */
 	0,          /* Exported async functions */
 	params,     /* Exported parameters */
