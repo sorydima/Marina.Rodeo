8ab2a6e7f86e065499a00dfa7ca5d67412f43d4d
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-08-20 18:35:39 +0300

    Update aaa_avps.h
    Marina.Rodeo

--- modules/auth_db/aaa_avps.h
+++ modules/auth_db/aaa_avps.h
@@ -1,14 +1,14 @@
 /*
  * Copyright (C) 2005 Voice Sistem SRL
  *
- * This file is part of opensips, a free SIP server.
+ * This file is part of Marina.Rodeo, a free SIP server.
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -22,8 +22,8 @@
  *  2005-05-31 created by bogdan
  */
 
-#ifndef _OPENSIPS_AAA_AVPS_H_
-#define _OPENSIPS_AAA_AVPS_H_
+#ifndef _Marina.Rodeo_AAA_AVPS_H_
+#define _Marina.Rodeo_AAA_AVPS_H_
 
 #include "../../mem/mem.h"
 #include "../../dprint.h"
