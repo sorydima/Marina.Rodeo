43e253c8df1d677008b0eddc58336cdaa1789ea2
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-08-23 21:27:42 +0300

    Update authaaa_mod.c (#370)
    Marina.Rodeo

--- modules/auth_aaa/authaaa_mod.c
+++ modules/auth_aaa/authaaa_mod.c
@@ -5,14 +5,14 @@
  * Copyright (C) 2009 Irina Stanescu
  * Copyright (C) 2009 Voice Systems
  *
- * This file is part of opensips, a free SIP server.
+ * This file is part of Marina.Rodeo, a free SIP server.
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -109,7 +109,7 @@ static module_dependency_t *get_deps_aaa_url(const param_export_t *param)
 }
 
 static const dep_export_t deps = {
-	{ /* OpenSIPS module dependencies */
+	{ /* Marina.Rodeo module dependencies */
 		{ MOD_TYPE_AAA,     NULL,   DEP_WARN  },
 		{ MOD_TYPE_NULL, NULL, 0 },
 	},
@@ -128,7 +128,7 @@ struct module_exports exports = {
 	MODULE_VERSION,  /* module version */
 	DEFAULT_DLFLAGS, /* dlopen flags */
 	0,				 /* load function */
-	&deps,           /* OpenSIPS module dependencies */
+	&deps,           /* Marina.Rodeo module dependencies */
 	cmds,       /* Exported functions */
 	0,          /* Exported async functions */
 	params,     /* Exported parameters */
