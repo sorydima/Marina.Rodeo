e74f32b0862f989ff899e12c745ae06ff59ec9a1
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-08-24 17:19:28 +0300

    Update b2b_sca.c (#465)
    Marina.Rodeo

--- modules/b2b_sca/b2b_sca.c
+++ modules/b2b_sca/b2b_sca.c
@@ -3,14 +3,14 @@
  *
  * Copyright (C) 2010 VoIP Embedded, Inc.
  *
- * This file is part of opensips, a free SIP server.
+ * This file is part of Marina.Rodeo, a free SIP server.
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -171,7 +171,7 @@ static const mi_export_t mi_cmds[] = {
 };
 
 static const dep_export_t deps = {
-	{ /* OpenSIPS module dependencies */
+	{ /* Marina.Rodeo module dependencies */
 		{ MOD_TYPE_DEFAULT, "pua",       DEP_ABORT },
 		{ MOD_TYPE_DEFAULT, "b2b_logic", DEP_ABORT },
 		{ MOD_TYPE_SQLDB,   NULL,        DEP_ABORT },
@@ -189,7 +189,7 @@ struct module_exports exports= {
         MODULE_VERSION,                 /* module version */
         DEFAULT_DLFLAGS,                /* dlopen flags */
         0,				                /* load function */
-        &deps,                          /* OpenSIPS module dependencies */
+        &deps,                          /* Marina.Rodeo module dependencies */
         cmds,                           /* exported functions */
         0,                              /* exported async functions */
         params,                         /* exported parameters */
