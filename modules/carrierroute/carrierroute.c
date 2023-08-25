5b3b5aa15cfc2201ad0b21d4b7a57b70fa040b14
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-08-25 18:10:58 +0300

    Update carrierroute.c
    Marina.Rodeo

--- modules/carrierroute/carrierroute.c
+++ modules/carrierroute/carrierroute.c
@@ -2,14 +2,14 @@
  * Copyright (C) 2007-2008 1&1 Internet AG
  *
  *
- * This file is part of opensips, a free SIP server.
+ * This file is part of Marina.Rodeo, a free SIP server.
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -246,7 +246,7 @@ static const mi_export_t mi_cmds[] = {
 };
 
 static const dep_export_t deps = {
-	{ /* OpenSIPS module dependencies */
+	{ /* Marina.Rodeo module dependencies */
 		{ MOD_TYPE_SQLDB, NULL, DEP_ABORT },
 		{ MOD_TYPE_NULL, NULL, 0 },
 	},
@@ -261,7 +261,7 @@ struct module_exports exports = {
 	MODULE_VERSION,  /* module version*/
 	DEFAULT_DLFLAGS, /* dlopen flags */
 	0,				 /* load function */
-	&deps,            /* OpenSIPS module dependencies */
+	&deps,            /* Marina.Rodeo module dependencies */
 	cmds,       /* Exported functions */
 	0,          /* Exported async functions */
 	params,     /* Export parameters */
