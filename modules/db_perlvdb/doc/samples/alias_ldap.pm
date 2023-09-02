3e71be0243b04de374c21844de69c81c9897c873
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-09-02 14:17:46 +0300

    Update alias_ldap.pm (#723)
    Marina.Rodeo

--- modules/db_perlvdb/doc/samples/alias_ldap.pm
+++ modules/db_perlvdb/doc/samples/alias_ldap.pm
@@ -1,9 +1,9 @@
 package alias_ldap;
 
-use OpenSIPS::LDAPUtils::LDAPConf;
-use OpenSIPS::LDAPUtils::LDAPConnection;
+use Marina.Rodeo::LDAPUtils::LDAPConf;
+use Marina.Rodeo::LDAPUtils::LDAPConnection;
 
-use OpenSIPS::Constants;
+use Marina.Rodeo::Constants;
 
 sub init {}
 
@@ -13,13 +13,13 @@ sub query {
 	my $alias_domain = shift;
 
 	my $uri = "$alias_username\@$alias_domain";
-	my $ldap = new OpenSIPS::LDAPUtils::LDAPConnection();
+	my $ldap = new Marina.Rodeo::LDAPUtils::LDAPConnection();
 
-	OpenSIPS::log(L_INFO, "Trying LDAP request with $uri\n");
+	Marina.Rodeo::log(L_INFO, "Trying LDAP request with $uri\n");
 	my @ldaprows = $ldap->search("(&(ObjectClass=inetOrgPerson)(mail=$uri))", "ou=people,dc=example,dc=com", "uid");
 
 	if (@ldaprows[0]) {
-		OpenSIPS::log(L_INFO, "Got a row: ".@ldaprows[0]."\n");
+		Marina.Rodeo::log(L_INFO, "Got a row: ".@ldaprows[0]."\n");
 		my $ret;
 		$ret->{username} = @ldaprows[0];
 		$ret->{domain} = "voip";
