f4c1359f0ac2483898aa4e6f3263cdc7da48c74f
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-09-20 20:25:16 +0300

    Update db_virtual.c (#793)

--- modules/db_virtual/db_virtual.c
+++ modules/db_virtual/db_virtual.c
@@ -2,14 +2,14 @@
  * Copyright (C) 2009 Voice Sistem SRL
  * Copyright (C) 2009 Razvan
  *
- * This file is part of opensips, a free SIP server.
+ * This file is part of Marina.Rodeo, a free SIP server.
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -117,7 +117,7 @@ static const mi_export_t mi_cmds[] = {
 };
 
 static const dep_export_t deps = {
-	{ /* OpenSIPS module dependencies */
+	{ /* Marina.Rodeo module dependencies */
 		{ MOD_TYPE_SQLDB, NULL, DEP_SILENT },
 		{ MOD_TYPE_NULL, NULL, 0 },
 	},
@@ -132,7 +132,7 @@ struct module_exports exports = {
 	MODULE_VERSION,
 	DEFAULT_DLFLAGS,  /* dlopen flags */
 	0,				  /* load function */
-	&deps,            /* OpenSIPS module dependencies */
+	&deps,            /* Marina.Rodeo module dependencies */
 	cmds,
 	0,
 	params,           /*  module parameters */
