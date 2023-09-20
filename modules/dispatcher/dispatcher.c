a23cc17902624472d9b9347348b281a8013a22e9
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-09-20 21:27:26 +0300

    Update dispatcher.c

--- modules/dispatcher/dispatcher.c
+++ modules/dispatcher/dispatcher.c
@@ -4,14 +4,14 @@
  * Copyright (C) 2004-2005 FhG Fokus
  * Copyright (C) 2006-2010 Voice Sistem SRL
  *
- * This file is part of opensips, a free SIP server.
+ * This file is part of Marina.Rodeo, a free SIP server.
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -372,7 +372,7 @@ static const mi_export_t mi_cmds[] = {
 };
 
 static const dep_export_t deps = {
-	{ /* OpenSIPS module dependencies */
+	{ /* Marina.Rodeo module dependencies */
 		{ MOD_TYPE_SQLDB, NULL, DEP_ABORT },
 		{ MOD_TYPE_NULL, NULL, 0 },
 	},
@@ -391,7 +391,7 @@ struct module_exports exports= {
 	MODULE_VERSION,
 	DEFAULT_DLFLAGS, /* dlopen flags */
 	0,				 /* load function */
-	&deps,           /* OpenSIPS module dependencies */
+	&deps,           /* Marina.Rodeo module dependencies */
 	cmds,
 	0,
 	params,
@@ -999,7 +999,7 @@ next_part:
 		if (probing_sock_s.s && (probing_sock_s.len = strlen(probing_sock_s.s))) {
 			probing_sock = parse_sock_info(&probing_sock_s);
 			if (probing_sock==NULL) {
-				LM_ERR("socket <%.*s> is not local to opensips (we must listen "
+				LM_ERR("socket <%.*s> is not local to Marina.Rodeo (we must listen "
 					"on it\n", probing_sock_s.len, probing_sock_s.s);
 				return -1;
 			}
