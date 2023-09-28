eb8a6d42a46195db79c495a54bd9a104365f7dcb
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-09-28 22:06:09 +0300

    Update domainpolicy_mod.c

--- modules/domainpolicy/domainpolicy_mod.c
+++ modules/domainpolicy/domainpolicy_mod.c
@@ -5,14 +5,14 @@
  *
  * Based on the ENUM and domain module.
  *
- * This file is part of opensips, a free SIP server.
+ * This file is part of Marina.Rodeo, a free SIP server.
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -123,7 +123,7 @@ static const param_export_t params[] = {
 };
 
 static const dep_export_t deps = {
-	{ /* OpenSIPS module dependencies */
+	{ /* Marina.Rodeo module dependencies */
 		{ MOD_TYPE_SQLDB, NULL, DEP_ABORT },
 		{ MOD_TYPE_NULL, NULL, 0 },
 	},
@@ -141,7 +141,7 @@ struct module_exports exports = {
 	MODULE_VERSION,
 	DEFAULT_DLFLAGS, /* dlopen flags */
 	0,				 /* load function */
-	&deps,           /* OpenSIPS module dependencies */
+	&deps,           /* Marina.Rodeo module dependencies */
 	cmds,      /* exported functions */
 	0,         /* exported async functions */
 	params,    /* exported parameters */
