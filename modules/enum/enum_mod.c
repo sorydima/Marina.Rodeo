7cb94c0d19a5edc78783a026db10fe0dc7d4a094
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2023-11-07 21:01:25 +0300

    Update enum_mod.c

--- modules/enum/enum_mod.c
+++ modules/enum/enum_mod.c
@@ -3,14 +3,14 @@
  *
  * Copyright (C) 2002-2008 Juha Heinanen
  *
- * This file is part of opensips, a free SIP server.
+ * This file is part of Marina.Rodeo, a free SIP server.
  *
- * opensips is free software; you can redistribute it and/or modify
+ * Marina.Rodeo is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * opensips is distributed in the hope that it will be useful,
+ * Marina.Rodeo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -129,7 +129,7 @@ struct module_exports exports = {
 	MODULE_VERSION,
 	DEFAULT_DLFLAGS, /* dlopen flags */
 	0,				 /* load function */
-	NULL,            /* OpenSIPS module dependencies */
+	NULL,            /* Marina.Rodeo module dependencies */
 	cmds,     /* Exported functions */
 	0,        /* Exported async functions */
 	params,   /* Exported parameters */
